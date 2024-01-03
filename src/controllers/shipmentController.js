f62f6617bb5a90511bd970fe7f6ca429226cc7e5
Alessandro Marchesin <alessandromarchesin@gmail.com>
2024-01-03 16:29:02 +0100

    Added shipment management

--- src/controllers/shipmentController.js
+++ src/controllers/shipmentController.js
@@ -1,14 +1,25 @@
-const { addShipment, getShipments } = require('../services/shipmentService');
+const { addShipment, getShipments } = require("../services/shipmentService");
 
-const getShipmentsEndpoint = async(req, res) => {
-    // TODO: Implement method
+const getShipmentsEndpoint = async (req, res) => {
+    const { success, shipment, error } = await getShipments();
+    if (success) {
+        res.status(200).json(shipment);
+    } else {
+        res.status(500).json({ error });
+    }
 };
 
-const addShipmentEndpoint = async(req, res) => {
-    // TODO: Implement method
+const addShipmentEndpoint = async (req, res) => {
+    const { distance_km, price } = req.body;
+    const { success, id, error } = await addProduct(distance_km, price);
+    if (success) {
+        res.status(201).json(id);
+    } else {
+        res.status(500).json({ error });
+    }
 };
 
-module.exports = { 
-    getShipmentsEndpoint, 
-    addShipmentEndpoint 
+module.exports = {
+    getShipmentsEndpoint,
+    addShipmentEndpoint,
 };
--- src/services/shipmentService.js
+++ src/services/shipmentService.js
@@ -1,14 +1,32 @@
-const { insertQuery, selectQuery } = require('../config/configDB');
+const { insertQuery, selectQuery } = require("../config/configDB");
 
-const getShipments = async() => {
-    // TODO: Implement method
+const getShipments = async () => {
+    const query = "SELECT * FROM Shipments";
+    try {
+        const shipment = await selectQuery(query);
+        return { success: true, result: shipment };
+    } catch {
+        return {
+            success: false,
+            error,
+        };
+    }
 };
 
-const addShipment = async(shipment) => {
-    // TODO: Implement method
+const addShipment = async (shipment) => {
+    const { distance_km, price } = shipment;
+    const query = `INSERT INTO Products (distance_km, price) VALUES ('${distance_km}, ${price});`;
+    try {
+        let id = await insertQuery(query);
+        return { success: true, id };
+    } catch {
+        {
+            success: false, error;
+        }
+    }
 };
 
 module.exports = {
     getShipments,
-    addShipment
+    addShipment,
 };
