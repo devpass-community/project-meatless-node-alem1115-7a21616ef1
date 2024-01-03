79b2db2336d94f07eef1c1e672a184481d73e7a7
Alessandro Marchesin <alessandromarchesin@gmail.com>
2024-01-03 17:20:54 +0100

    add order feature

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,11 +1,22 @@
 const { addOrder, getOrders } = require("../services/orderService");
 
 const getOrdersEndpoint = async (req, res) => {
-    // TODO: Implement method
+    const { success, order, error } = await getOrders();
+    if (success) {
+        res.status(200).json(order);
+    } else {
+        res.status(500).json({ error });
+    }
 };
 
 const addOrderEndpoint = async (req, res) => {
-    // TODO: Implement method
+    const { restaurant_id, subtotal, freight, total_price, user_id } = req.body;
+    const { success, id, error } = await addOrder(restaurant_id, subtotal, freight, total_price, user_id);
+    if (success) {
+        res.status(201).json(id);
+    } else {
+        res.status(500).json({ error });
+    }
 };
 
 module.exports = {
