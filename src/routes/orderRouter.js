216dd5f6a3b64f949e63b5f98a1eaa652e80d766
Alessandro Marchesin <alessandromarchesin@gmail.com>
2024-03-18 16:36:44 +0100

    Update orderRouter.js

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const { addOrderEndpoint, getOrdersEndpoint } = require("../controllers/orderController");
 
 const orderRouter = Router();
-productRouter.get("/products", getOrdersEndpoint);
-productRouter.post("/products", addOrderEndpoint);
+productRouter.get("/orders", getOrdersEndpoint);
+productRouter.post("/orders", addOrderEndpoint);
 
 module.exports = { orderRouter };
